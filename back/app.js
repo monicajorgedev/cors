51db10a499bf360041306bf663346f04d3c079f8
monicajorgedev <monicajorge.dev@gmail.com>
2024-09-11 13:01:16 +0200

    con clase resuelta

--- back/app.js
+++ back/app.js
@@ -3,11 +3,16 @@ const app = express()
 const axios = require("axios")
 const cors = require("cors")
 
-app.use(cors())
+app.use(cors(
+    //{
+    //origin: ['http://127.0.0.1:5500/', 'https://google.es']
+    //}
+))
 
 const url = "https://rickandmortyapi.com/api/character"
 
-app.get("/characters", async (req,res) => {
+app.get("/characters", async (req,res) => 
+    {
     try {
         const response = await axios.get(url);
         let data = []
@@ -19,7 +24,7 @@ app.get("/characters", async (req,res) => {
         
         res.json(data)
     } catch (err){ 
-        res.status(404).json({error: "personaje no encontrado"})
+        res.status(500).json({error: `personaje no encontrado, ${err}`})
     }
 })
 
@@ -33,9 +38,13 @@ app.get("/characters/:name", async (req,res) => {
             const responsePage = await axios.get(`${url}?name=${name}&page=${i}`)
             data.push(...responsePage.data.results)
         }
-        res.json(data)
+        const characterList = data.map(character => {
+            const {name, status, gender, species, image, origin: {name: origin}} = character
+            return {name, status, gender, species, image, origin}
+        })
+        res.json(characterList)
     } catch (err){ 
-        res.status(404).json({error: "personaje no encontrado"})
+        res.status(500).json({error: `personaje no encontrado, ${err}`})
 
     }
 })
--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+
 function getCharacterInfo(){
     const characterName = document.getElementById("characterName")
     const characterInfo = document.getElementById("characterInfo")
@@ -27,3 +28,28 @@ function getCharacterInfo(){
     })
     .catch (error => characterInfo.innerHTML = "<p> Imposible acceder al personaje</p>")
 }
+
+/*
+try {
+    const response = await fetch(`http://localhost:3000/characters/${character}`)
+    const data = await response.json()
+    characterInfo.innerHTML = ""
+        data.map( character => {
+             const {name, status, gender, species, image, origin} = character
+             return
+            const template = `<div>
+                <ul>
+                <li>Name: ${name}</li>
+                <li>Status:  ${status}</li>
+                <li>Species: ${species}</li>
+                <li>Gender: ${gender}</li>
+                <li>Origin: ${origin}</li>
+                </ul>
+                <img src="${image}" alt="${name}"/>
+                </div>` 
+            characterInfo.innerHTML += template
+
+} catch (err) {
+error => characterInfo.innerHTML = "<p> Imposible acceder al personaje</p>"
+}
+*/
